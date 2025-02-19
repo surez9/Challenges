22c756c1ecfa594f8281c61f3b00d34d220168e9
Suresh Bhandari <43695320+surez9@users.noreply.github.com>
2025-02-19 20:33:17 +0545

    Challenge 2

---
+++ challenges/src/solutions/Question2.java
@@ -0,0 +1,35 @@
+package solutions;
+
+import java.util.Set;
+
+public class Question2 {
+    public static void main(String[] args) {
+        String S = "abpplee";
+        Set<String> D = Set.of("able","ale","apple","bale","kangaroo");
+
+        String result = longestSubSequence(S,D);
+        System.out.println(result);
+    }
+
+    public static String longestSubSequence(String S, Set<String> D) {
+        String longestString = "";
+
+        for (String str : D) {
+            if(isSubsequence(S,str) && str.length()>longestString.length()){
+                longestString = str;
+            }
+        }
+        return longestString;
+    }
+
+    public static boolean isSubsequence(String S, String T) {
+        int i = 0, j = 0;
+        while (i < S.length() && j < T.length()) {
+            if (S.charAt(i) == T.charAt(j)) {
+                j++;
+            }
+            i++;
+        }
+        return j == T.length();
+    }
+}
