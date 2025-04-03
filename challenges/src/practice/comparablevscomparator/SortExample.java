14d3a3349e126c0e9b601ad1877cf657c664b2a9
Suresh Bhandari <suresh.bhandari@cotiviti.com>
2025-04-03 20:30:00 +0545

    Comparator vs Comparable practice

---
+++ challenges/src/practice/comparablevscomparator/SortExample.java
@@ -0,0 +1,29 @@
+package practice.comparablevscomparator;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+
+public class SortExample {
+    public static void main(String[] args) {
+
+        ArrayList<Integer> intList = new ArrayList<>();
+        intList.add(3);
+        intList.add(35);
+        intList.add(10);
+        System.out.println(intList);
+
+        Collections.sort(intList);
+        System.out.println(intList);
+
+        ArrayList<Emp> emps = new ArrayList<>();
+        emps.add(new Emp("Suresh","83319",3));
+        emps.add(new Emp("Susan","832349",1));
+        emps.add(new Emp("Harka","7483",2));
+
+        System.out.println(emps);   // Print hashcode if ToString() is not override in Emp class
+
+        // Collections.sort(emps);  -- gives error as the object type cannot use comparable
+
+    }
+}
